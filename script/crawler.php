28277dc4393ba63989de2539e597f9f5a4bd1414
peter279k <peter279k@gmail.com>
2016-08-05 23:25:59 +0800

    add crawler.php

--- script/crawler.php
+++ script/crawler.php
@@ -1,8 +1,14 @@
 <?php
 	require __DIR__ . "/vendor/autoload.php";
-	require "./php/CnnectDB.php";
+	require "CnnectDB.php";
 	
-	$client = new Guzzle\HttpClient;
+	use GuzzleHttp\Client;
+	use GuzzleHttp\Exception\RequestException;
+	
+	$client = new Client;
+	
+	$connection = new ConnectDB();
+	$conn = $connection -> InitialDB();
 	
 	$AppToken = "";
 	
@@ -20,8 +26,52 @@
 	}
 	
 	$contents = array();
+	$PageID = array("1450930895146846");
 	
-	$response = $client -> get("https://graph.facebook.com/1450930895146846/feed?access_token=" . $AppToken);
+	$response = $client -> get("https://graph.facebook.com/" . $PageID[0] . "/feed?access_token=" . $AppToken);
 	
-	$DomCrawler = new 
+	$page = json_decode($response -> getBody(), true);
+	
+	$count = count($page["data"]);
+	
+	while($count !== 0) {
+		for($CountData=0;$CountData<$count;$CountData++) {
+			if(isset($page["data"][$CountData]["message"]))
+				$message = $page["data"][$CountData]["message"];
+			else
+				$message = "";
+			if(isset($page["data"][$CountData]["object_id"]))
+				$ObjectID = $page["data"][$CountData]["object_id"];
+			else
+				$ObjectID = "";
+			if(isset($page["data"][$CountData]["created_time"]))
+				$CreatedTime = $page["data"][$CountData]["created_time"];
+			else
+				$CreatedTime = "";
+			
+			$data["message"] = str_replace("☺ ", "", trim($message));
+			$data["message"] = str_replace("\n", "", $data["message"]);
+			$data["object_id"] = $ObjectID;
+			$data["created_time"] = $CreatedTime;
+			
+			if($page_arr[$counter] === $PageID[0])
+				$sql = "INSERT INTO beauty_nttu(message,obj_id,created_time) VALUES(:message,:object_id,:created_time)";
+			
+			$result = $connection -> ProcessData($conn, $sql, $data, "insert-record");
+			
+			switch($result) {
+				case "insert-fail":
+					file_put_contents("log/error.log", "insert-fail,".$message.",".$ObjectID."\r\n", FILE_APPEND);
+					break;
+				case "duplicate-entry":
+					file_put_contents("log/error.log", "duplicate-entry,".$message.",".$ObjectID."\r\n", FILE_APPEND);
+					break;
+			}
+		}
+		
+		$page = $client -> get($page["paging"]["next"]);
+		$page = json_decode($response -> getBody(), true);
+		
+		$count = count($page["data"]);
+	}
 ?>
