9a3453b6d0de3a817fb2813efb3d209f7ad87a66
peter279k <peter279k@gmail.com>
2016-08-05 23:51:19 +0800

    modify require path

--- script/crawler.php
+++ script/crawler.php
@@ -1,7 +1,7 @@
 <?php
 	set_time_limit(0);
 	ini_set("display_errors", "On");
-	require __DIR__ . "/vendor/autoload.php";
+	require "../vendor/autoload.php";
 	require "CnnectDB.php";
 	
 	use GuzzleHttp\Client;
