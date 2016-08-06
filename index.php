471f520fedf028ff87c85da9de216b2f925b864a
peter279k <peter279k@gmail.com>
2016-08-06 18:37:55 +0800

    change instance framework

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 	require "vendor/autload.php";
 	require "script/ConnectDB.php";
-	ini_set("display_errors", "On");
 	
-	Flight::route('GET /@FileName', function($FileName) {
+	use flight\Engine;
+	
+	$app = new Engine();
+	
+	$app -> route('GET /@FileName', function($FileName) {
 		if($FileName === "index") {
 			$sql = "SELECT `message`, `obj_id`, `created_time` FROM  `beauty_nttu`  WHERE `message` LIKE '%【正妹】%'";
 			$conn = new ConnectDB();
@@ -22,51 +25,51 @@
 			}
 		
 			//echo $str;
-			Flight::render('index_view.php', array('data' => $str));
-			//Flight::redirect('./index_view.php', 401);
+			$app -> render('index_view.php', array('data' => $str));
+			//$app -> redirect('./index_view.php', 401);
 		}
 	});
 	
-	Flight::route('GET /bower_components/bootswatch/flatly/@FileName', function($FileName) {
+	$app -> route('GET /bower_components/bootswatch/flatly/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /css/@FileName', function($FileName) {
+	$app -> route('GET /css/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /bower_components/jquery/dist/@FileName', function($FileName) {
+	$app -> route('GET /bower_components/jquery/dist/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /bower_components/bootstrap/dist/js/@FileName', function($FileName) {
+	$app -> route('GET /bower_components/bootstrap/dist/js/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /bower_components/lazyloadxt/dist/@FileName', function($FileName) {
+	$app -> route('GET /bower_components/lazyloadxt/dist/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /bower_components/swipebox/src/js/@FileName', function($FileName) {
+	$app -> route('GET /bower_components/swipebox/src/js/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::route('GET /js/@FileName', function($FileName) {
+	$app -> route('GET /js/@FileName', function($FileName) {
 		$FileName = htmlentities($FileName, "utf-8");
 		echo @file_get_contents($FileName);
 	});
 	
-	Flight::map('notFound', function() {
+	$app -> map('notFound', function() {
 		header("HTTP/1.1 404 Not found");
 		header("Content-Type: text/html; charset=utf-8");
 		echo "<h2>Not found</h2>";
 	});
 
-	Flight::start();
+	$app -> start();
 ?>
