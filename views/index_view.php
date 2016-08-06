588f721a1c9fec92af057e7f95c907f9ae30134f
peter279k <peter279k@gmail.com>
2016-08-06 21:50:34 +0800

    mv js file position

--- views/index_view.php
+++ views/index_view.php
@@ -9,31 +9,32 @@
 	<title>表特牆</title>
 	<link ref="bower_components/bootswatch/flatly/bootstrap.min.css">
 	<link ref="css/index.css">
+</head>
+<body>
+	<div class="container">
+		<div id="main_area">
+			<!-- Slider -->
+			<div class="row">
+				<div class="col-sm-12">
+					<!-- Bottom switcher of slider -->
+					<ul class="hide-bullets">
+						<li class="col-sm-3">
+							<a class="thumbnail" id="carousel-selector-0">
+								<img src="http://placehold.it/150x150&text=zero">
+							</a>
+						</li>
+					</ul>
+				</div>
+				<!--/Slider-->
+			</div>
+
+		</div>
+	</div>
+	
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	<script src="bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 	<script src="bower_components/swipebox/src/js/jquery.swipebox.min.js"></script>
 	<script src="js/index.js"></script>
-</head>
-<body>
-	<div class="container">
-    <div id="main_area">
-        <!-- Slider -->
-        <div class="row">
-            <div class="col-sm-12">
-                <!-- Bottom switcher of slider -->
-                <ul class="hide-bullets">
-					<li class="col-sm-3">
-                        <a class="thumbnail" id="carousel-selector-0">
-                            <img src="http://placehold.it/150x150&text=zero">
-                        </a>
-                    </li>
-				</ul>
-            </div>
-            <!--/Slider-->
-        </div>
-
-    </div>
-</div>
 </body> 
 </html>
