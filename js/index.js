70ad2fd56b4b5b6e15b5ccaee4ee6900c906013a
peter279k <peter279k@gmail.com>
2016-08-07 01:13:51 +0800

    add message the image is missing

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ $(function() {
 	
 	$( 'img' ).bind('error', function(ev) {
 		//error has been thrown
+		var id = $(this).attr('src');
+		console.log("image is missing... " + id);
 		$(this).remove();
 	});
 });
